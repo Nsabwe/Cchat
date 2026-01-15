c55b0ac92263b89dd492034b2e28f32f0b438092
Spaida Nsabwe <spaidaliphava@gmail.com>
2026-01-15 09:21:52 +0000

    Add files via upload
    Signed-off-by: Spaida Nsabwe <spaidaliphava@gmail.com>

---
+++ user.js
@@ -0,0 +1,10 @@
+// models/User.js
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  profile: { type: String, default: "" },
+  socketId: { type: String, default: "" }
+}, { timestamps: true });
+
+module.exports = mongoose.model("User", userSchema);
